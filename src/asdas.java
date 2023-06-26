ca5a8ad33370bbd7be75769ed2f07c2588147e95
asad1231 <zavia@naver.com>
2023-06-26 21:40:52 +0900

    0622

---
+++ src/asdas.java
@@ -0,0 +1,16 @@
+
+
+class ConsTest{
+    int a;
+   
+    ConsTest(int a){
+      this.a=a;
+      System.out.println("a="+a);
+    }
+}//CosTest class
+
+public class asdas{
+  public static void main(String[] args){
+      new ConsTest(10);
+  }
+}
